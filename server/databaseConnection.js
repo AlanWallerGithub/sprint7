15c9c3557fd6af736a65de1daeedd04aa301e839
AlanWallerGithub <140154835+AlanWallerGithub@users.noreply.github.com>
2024-01-08 18:31:40 +0100

    pc changes

--- server/databaseConnection.js
+++ server/databaseConnection.js
@@ -1,14 +1,48 @@
-import mongo from 'mongodb';
+var MongoClient = require('mongodb').MongoClient;
 
-let db = mongo.MongoClient;
 
-console.log('ups')
+var url = "mongodb://localhost:27017/";
 
-db.connect('mongodb://root:password@localhost:27017/?authMechanism=DEFAULT', function(err, dbData){
-    if (err){
-        console.log('MongoDB not connected...')
-        throw err;
-        
-    }
-    console.log('MongoDB connected...')
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  var myobj = { name: "Company Inc", address: "Highway 37" };
+  dbo.collection("customers").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
 });
+// async function main(){
+
+
+
+   
+ 
+
+//     const client = new MongoClient(uri);
+ 
+//     try {
+//         // Connect to the MongoDB cluster
+//         await client.connect();
+ 
+//         // Make the appropriate DB calls
+//         await  listDatabases(client);
+ 
+//     } catch (e) {
+//         console.error(e);
+//     } finally {
+//         await client.close();
+//     }
+// }
+
+// main().catch(console.error);
+
+// let databasesList;
+
+// async function listDatabases(client){
+//     databasesList = await client.db().admin().listDatabases();
+ 
+//     console.log("Databases:");
+//     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+// };
